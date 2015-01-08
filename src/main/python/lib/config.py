b1cb7020cada2c23d5c950c2eeaace67c23f3cc2
Ken W <ken@qualia-media.com>
2015-01-08 16:43:58 -0500

    fixed the config build issue

--- src/main/python/lib/config.py
+++ src/main/python/lib/config.py
@@ -23,19 +23,68 @@ from lib.singleton import Singleton
 from lib.utils import get_base, real_lines, debug
 from os.path import abspath, join, dirname, isfile
 
+
+@Singleton
+class Config():
+    
+    _mode = ''
+    _config_path = get_base('config/setup.yaml') 
+    _version_file_path = join(abspath(dirname('__file__')), 'doc/version.txt')
+    
+    def __init__ (self):
+        self._mode = self.get_yaml_data(['common', 'run_mode'], 'dev')  
+    
+    def get_category_path(self):
+        if self._mode == 'prod' or self._mode == 'production':
+            return 'config/category-production.txt'
+        else:
+            return 'config/category.txt'
+        
+    def get_mode(self):
+        if not self._mode:
+            self.__init__()
+        return self._mode
+    
+    def set_mode(self, m):
+        self._mode = m
+    
+    def set_config_path(self, c_p):
+        self._config_path = c_p
+    
+    def get_config_path(self):
+        return self._config_path
+    
+    def get_version_file_path(self):
+        return self._version_file_path
+    
+    def get_yaml_data(self, keys, default=''):
+        yaml_file=None
+        try:
+            yaml_file = open(self.get_config_path(), 'r')
+            yaml_obj = load(yaml_file.read())        
+            current_tree = yaml_obj
+            for key in keys:
+                current_tree = current_tree[key]
+            result = current_tree
+        except Exception as e:
+            result = default
+        finally:
+            if yaml_file:
+                yaml_file.close()
+        return result
+    
+
 @Singleton
 class VersionHistory(Loggable):
 
-    _version_file = None
+    _version_file = Config.Instance().get_version_file_path()
     _current_version = None
     _version_obj=list()
 
     def __init__(self):
-        self._version_file = join(abspath(dirname('__file__')), 'doc/version.txt')
-        #self.info("-__file__:" + dirname(__file__)
-        self._parse()
+        pass
 
-    def _parse(self):
+    def parse(self):
         #self.info("--_version_file:" + self._version_file)
         lines = real_lines(self._version_file)
         firstItem=True
@@ -62,48 +111,3 @@ def get_software_version():
 
 def get_all_versions():
     return _ALL_VERSIONS;
-
-@Singleton
-class Config():
-    
-    _mode = ''
-    _config_path = get_base('config/setup.yaml') 
-    
-    def __init__ (self):
-        self._mode = self.get_yaml_data(['common', 'run_mode'], 'dev')  
-    
-    def get_category_path(self):
-        if self._mode == 'prod' or self._mode == 'production':
-            return 'config/category-production.txt'
-        else:
-            return 'config/category.txt'
-        
-    def get_mode(self):
-        if not self._mode:
-            self.__init__()
-        return self._mode
-    
-    def set_mode(self, m):
-        self._mode = m
-    
-    def set_config_path(self, c_p):
-        self._config_path = c_p
-    
-    def get_config_path(self):
-        return self._config_path
-    
-    def get_yaml_data(self, keys, default=''):
-        yaml_file=None
-        try:
-            yaml_file = open(self.get_config_path(), 'r')
-            yaml_obj = load(yaml_file.read())        
-            current_tree = yaml_obj
-            for key in keys:
-                current_tree = current_tree[key]
-            result = current_tree
-        except Exception as e:
-            result = default
-        finally:
-            if yaml_file:
-                yaml_file.close()
-        return result
