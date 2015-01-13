9a8b8044253481c9c8f436456aca9b977b0e7ebd
Ken Wu <ken_11223@yahoo.com>
2015-01-12 23:03:14 -0500

    beta 5

--- src/main/python/web/cache.py
+++ src/main/python/web/cache.py
@@ -19,9 +19,10 @@
 # Date: 2014 Dec - 2015
 
 import os
-from lib.loggable import Loggable;
+from lib.loggable import Loggable
 from lib.singleton import Singleton
-from lib.utils import debug, convert_datetime_to_s
+from lib.utils import debug, convert_datetime_to_s,add_seconds_to_datetime
+from lib.config import Config
 from datetime import datetime
 from backend.database import SQLDatabase
 
@@ -30,15 +31,49 @@ time_since_restarted = convert_datetime_to_s(datetime.now()) + '+00:00 '+ os.env
 def get_server_time_since_last_restarted():
         return time_since_restarted
 
+class Cache_Field(object):
+    submissions_today = 'submissions_today'
+    submissions_in_the_past_n_days = 'submissions_in_the_past_n_days'
+
 @Singleton
 class Cache(Loggable):
     
     _sqldb = SQLDatabase.Instance();
+    _config = Config.Instance()
+    _cache = {}
+    _submissions_today_expir_cycle = None
+    _submissions_in_the_past_n_days_expir_cycle = None
+    _yaml_section = 'cache'
     
-    def get_submissions_today(self):
-        count = self._sqldb.get_aggregate_submission_count()
+    def __init__(self):
+        self._submissions_today_expir_cycle = Config.Instance().get_yaml_data([self._yaml_section, Cache_Field.submissions_today])
+        self._submissions_in_the_past_n_days_expir_cycle = Config.Instance().get_yaml_data([self._yaml_section, Cache_Field.submissions_in_the_past_n_days])
+        self.info('At Cache constructor...._submissions_today_expir_cycle: '+ self._submissions_today_expir_cycle)
+    
+    def get_submissions_today(self, refresh=False):
+        count = self._retrieve_from_cache(Cache_Field.submissions_today, self._submissions_today_expir_cycle)
+        if not count or refresh:
+             count = self._sqldb.get_aggregate_submission_count()   #call the corresponding underlying function
+             #self.info('refreshing from the cache on submissions_today: ' + count)
+             self._update_cache(Cache_Field.submissions_today, count)     #also update the cache layer
         return count
     
-    def get_submissions_in_the_past_n_days(self, back_days):
-        count = self._sqldb.get_aggregate_submission_count(back_days=back_days)
+    def get_submissions_in_the_past_n_days(self, back_days, refresh=False):
+        field_name=Cache_Field.submissions_in_the_past_n_days+'_'+str(back_days)
+        count = self._retrieve_from_cache(field_name, self._submissions_in_the_past_n_days_expir_cycle)
+        if not count or refresh:
+            count = self._sqldb.get_aggregate_submission_count(back_days=back_days)
+            self.info('refreshing from the cache on submissions_in_the_past_%s_days: %s'%(str(back_days),count))
+            self._update_cache(field_name, count)     #also update the cache layer
         return count
+    
+    def _retrieve_from_cache(self, field_name, interval_in_seconds):
+        if field_name in self._cache:
+            entry_created_time = self._cache[field_name][0]
+            entry_adjusted_time = add_seconds_to_datetime(entry_created_time, interval_in_seconds)
+            if entry_adjusted_time > datetime.now():
+                return self._cache[field_name][1]   #returns the value if it is still within the range
+        return None #else returns nothing
+    
+    def _update_cache(self, field_name, value):
+        self._cache[field_name] = (datetime.now(), value)   #simply update the cache
